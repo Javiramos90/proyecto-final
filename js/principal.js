7fc967aed75fe9053caf168b7324ba5eda8806f6
Javiramos90 <javirm170290@gmail.com>
2024-10-29 15:01:31 +0100

    seven commit

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,7 @@
 document.getElementById("img-buscar").addEventListener("click", function() {
     
     document.getElementById("input-buscar").classList.toggle("mostrar");
+    document.getElementById("btn-buscar").classList.toggle("mostrar");
 });
 
 document.getElementById("img-login").addEventListener("click", function() {
