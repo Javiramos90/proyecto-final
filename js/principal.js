87ac80d7e5130a6af10f7206333bb8e274c8b718
Javiramos90 <javirm170290@gmail.com>
2024-11-05 14:53:59 +0100

    nine commit

--- js/principal.js
+++ js/principal.js
@@ -1,23 +1,23 @@
-document.getElementById("img-buscar").addEventListener("click", function() {
-    
-    document.getElementById("input-buscar").classList.toggle("mostrar");
-    document.getElementById("btn-buscar").classList.toggle("mostrar");
+document.getElementById("img-buscar").addEventListener("click", function () {
+
+  document.getElementById("input-buscar").classList.toggle("mostrar");
+  document.getElementById("btn-buscar").classList.toggle("mostrar");
 });
 
 // document.getElementById("img-login").addEventListener("click", function() {
-    
+
 //     document.getElementById("input-login").classList.toggle("mostrar");
 // });
 
-document.getElementById("img-login").addEventListener("click", function(event) {
-    event.preventDefault(); // Evita que se recargue la página
-    const inputLogin = document.getElementById("input-login");
-    
-    // Alterna la clase "mostrar"
-    inputLogin.classList.toggle("visible");
-  });
+document.getElementById("img-login").addEventListener("click", function (event) {
+  event.preventDefault(); // Evita que se recargue la página
+  const inputLogin = document.getElementById("input-login");
 
-  // Selecciona todas las imágenes de la clase 'slide'
+  // Alterna la clase "mostrar"
+  inputLogin.classList.toggle("visible");
+});
+
+// Selecciona todas las imágenes de la clase 'slide'
 const slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
 
@@ -25,13 +25,13 @@ let currentSlide = 0;
 function cambiarSlide() {
   // Oculta la imagen actual
   slides[currentSlide].classList.remove('activo');
-  
+
   // Incrementa el índice para ir a la siguiente imagen
   currentSlide = (currentSlide + 1) % slides.length;
-  
+
   // Muestra la nueva imagen
   slides[currentSlide].classList.add('activo');
 }
 
 // Cambia la imagen cada 5 segundos (5000 ms)
- setInterval(cambiarSlide, 1000);
+setInterval(cambiarSlide, 3000);
